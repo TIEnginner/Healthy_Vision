ed71dbfa2328599869a04fb0032cbbd96804cfe6
Davi Luiz Teixeira Caboclo <daviltcaboclo@gmail.com>
2024-09-18 17:22:53 -0300

    Primeira versão ainda não funcional

---
+++ desktopV1.py
@@ -0,0 +1,164 @@
+import flet as ft
+#Precisa corrigir as cores
+
+class Message:
+    def __init__(self, user_name: str, text: str, message_type: str):
+        self.user_name = user_name
+        self.text = text
+        self.message_type = message_type
+
+
+class ChatMessage(ft.Row):
+    def __init__(self, message: Message):
+        super().__init__()
+        self.vertical_alignment = ft.CrossAxisAlignment.START
+        self.controls = [
+            ft.CircleAvatar(
+                content=ft.Text(self.get_initials(message.user_name)),
+                color=ft.colors.WHITE,
+                bgcolor=self.get_avatar_color(message.user_name),
+            ),
+            ft.Column(
+                [
+                    ft.Text(message.user_name, weight="bold", color=ft.colors.WHITE),
+                    ft.Text(message.text, selectable=True, color=ft.colors.WHITE),
+                ],
+                tight=True,
+                spacing=5,
+            ),
+        ]
+
+    def get_initials(self, user_name: str):
+        if user_name:
+            return user_name[:1].capitalize()
+        else:
+            return "U"  # Initial for unknown
+
+    def get_avatar_color(self, user_name: str):
+        colors_lookup = [
+            ft.colors.AMBER,
+            ft.colors.BLUE,
+            ft.colors.BROWN,
+            ft.colors.CYAN,
+            ft.colors.GREEN,
+            ft.colors.INDIGO,
+            ft.colors.LIME,
+            ft.colors.ORANGE,
+            ft.colors.PINK,
+            ft.colors.PURPLE,
+            ft.colors.RED,
+            ft.colors.TEAL,
+            ft.colors.YELLOW,
+        ]
+        return colors_lookup[hash(user_name) % len(colors_lookup)]
+
+
+def main(page: ft.Page):
+    page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
+    page.title = "Flet Chat"
+    page.bgcolor = ft.colors.BLACK  # Definindo a cor de fundo da página
+
+    def join_chat_click(e):
+        if not join_user_name.value:
+            join_user_name.error_text = "Name cannot be blank!"
+            join_user_name.update()
+        else:
+            page.session.set("user_name", join_user_name.value)
+            page.dialog.open = False
+            new_message.prefix = ft.Text(f"{join_user_name.value}: ", color=ft.colors.WHITE)
+            page.pubsub.send_all(
+                Message(
+                    user_name=join_user_name.value,
+                    text=f"{join_user_name.value} has joined the chat.",
+                    message_type="login_message",
+                )
+            )
+            page.update()
+
+    def send_message_click(e):
+        if new_message.value != "":
+            page.pubsub.send_all(
+                Message(
+                    page.session.get("user_name"),
+                    new_message.value,
+                    message_type="chat_message",
+                )
+            )
+            new_message.value = ""
+            new_message.focus()
+            page.update()
+
+    def on_message(message: Message):
+        if message.message_type == "chat_message":
+            m = ChatMessage(message)
+        elif message.message_type == "login_message":
+            m = ft.Text(message.text, italic=True, color=ft.colors.WHITE, size=12)
+        chat.controls.append(m)
+        page.update()
+
+    page.pubsub.subscribe(on_message)
+
+    # A dialog asking for a user display name
+    join_user_name = ft.TextField(
+        label="Enter your name to join the chat",
+        autofocus=True,
+        on_submit=join_chat_click,
+        bgcolor=ft.colors.GREY,
+        color=ft.colors.WHITE,
+    )
+    page.dialog = ft.AlertDialog(
+        open=True,
+        modal=True,
+        title=ft.Text("Welcome!", color=ft.colors.WHITE),
+        content=ft.Column([join_user_name], width=300, height=70, tight=True),
+        actions=[ft.ElevatedButton(text="Join chat", on_click=join_chat_click)],
+        actions_alignment=ft.MainAxisAlignment.END,
+    )
+
+    # Chat messages
+    chat = ft.ListView(
+        expand=True,
+        spacing=10,
+        auto_scroll=True,
+        bgcolor=ft.colors.BLACK,  # Cor de fundo do chat
+        padding=10,
+    )
+
+    # A new message entry form
+    new_message = ft.TextField(
+        hint_text="Write a message...",
+        autofocus=True,
+        shift_enter=True,
+        min_lines=1,
+        max_lines=5,
+        filled=True,
+        expand=True,
+        on_submit=send_message_click,
+        bgcolor=ft.colors.GRAY,
+        color=ft.colors.WHITE,
+    )
+
+    # Add everything to the page
+    page.add(
+        ft.Container(
+            content=chat,
+            border=ft.border.all(1, ft.colors.WHITE),
+            border_radius=5,
+            padding=10,
+            expand=True,
+        ),
+        ft.Row(
+            [
+                new_message,
+                ft.IconButton(
+                    icon=ft.icons.SEND_ROUNDED,
+                    tooltip="Send message",
+                    on_click=send_message_click,
+                    icon_color=ft.colors.WHITE,
+                ),
+            ]
+        ),
+    )
+
+
+ft.app(target=main)
